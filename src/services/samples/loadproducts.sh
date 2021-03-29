121fe733993dca9741b750f58470effdd7f01c4c
Dmitry Maslennikov <mrdaimor@gmail.com>
2021-03-29 11:05:20 +0300

    objectscript part of code

---
+++ src/services/samples/loadproducts.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+IRISDB="http://iris:52773"
+
+for jsonfile in *.json ;
+do
+    curl -d "@$jsonfile" -H "Content-Type: application/json" -X POST $IRISDB"/api/coffeeco/catalog/catalogproduct"
+done;
